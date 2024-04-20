e14971aaddc450e4b0f8c7e0ce9d30515b240ffd
Allnorm <ruallnorm@gmail.com>
2024-04-21 00:45:52 +0300

    Updated to version 3.3.3 1. Fixed bug "message thread not found"

--- main.py
+++ main.py
@@ -82,11 +82,12 @@ async def chatgpt(message: types.Message):
     await bot.send_chat_action(chat_id=message.chat.id, action='typing')
     answer = (await dialogs.get(context).get_answer(message, reply_msg, photo_base64)).split("\n\n")
     await message.reply(answer[0], allow_sending_without_reply=True)
+    thread_id = message.message_thread_id if message.is_topic_message else None
     for paragraph in answer[1::]:
         await asyncio.sleep(5)
         await bot.send_chat_action(chat_id=message.chat.id, action='typing')
         await asyncio.sleep(5)
-        await bot.send_message(message.chat.id, paragraph, message.message_thread_id)
+        await bot.send_message(message.chat.id, paragraph, thread_id)
 
 
 async def main() -> None:
